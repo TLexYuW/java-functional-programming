1eeef8371d412a97741dae83556e04c6417218fa
LexYu <84486286+TLexYuW@users.noreply.github.com>
2022-11-19 17:33:57 +0800

    combinator pattern

--- src/main/java/com/lex/combinatorpattern/CustomerValidatorService.java
+++ src/main/java/com/lex/combinatorpattern/CustomerValidatorService.java
@@ -16,4 +16,10 @@ public class CustomerValidatorService {
     public boolean isAdult(LocalDate birth){
         return Period.between(birth, LocalDate.now()).getYears() > 18;
     }
+
+    public boolean isValid(Customer customer){
+        return isEmailValid(customer.getEmail()) &&
+                isPhoneNumberValid(customer.getPhoneNumber()) &&
+                isAdult(customer.getBirth());
+    }
 }
